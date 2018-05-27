422f0d3604ade256bf33080bcdfc165a9724f751
Crooks5001 <Crooks5001@gmail.com>
2018-05-27 13:47:19 -0700

    simple classes added

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Jenkins test Project, starting out super plain.");
+        int add = simpleAddition(10,12);
+        int mult = simpleMultiplication(2,4);
+        int sub = simpleSubtraction(3,4);
+
+    }
+    public static int simpleAddition(int a, int b){
+        return a+b;
+    }
+    public static int simpleSubtraction(int a, int b){
+        return a-b;
+    }
+    public static int simpleMultiplication(int a, int b){
+        return a*b;
+    }
+}
